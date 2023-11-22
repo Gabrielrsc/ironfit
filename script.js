a5a038188875c404176db94eb0ac7f912d982b48
Gabriel Robertson <gabrielrobertson.s@gmail.com>
2023-11-21 23:45:10 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-const codigoBarras = document.getElementById("codigo-barras");
-const resultado = document.getElementById("resultado");
+function separaNumeros() {
+  // Obtém o texto do textarea de entrada
+  const textoEntrada = document.getElementById("textoEntrada").value;
 
-const enviar = () => {
-  const texto = codigoBarras.value;
+  // Converte o texto para um array de números
+  const numeros = textoEntrada.split(" ");
 
-  // Separar o texto em blocos de 9 dígitos
-  const blocos = texto.split("");
-  const tamanho = blocos.length;
+  // Cria um array vazio para armazenar os resultados
+  const resultados = [];
 
-  // Criar um array com os blocos formatados
-  const codigosFormatados = [];
-  for (let i = 0; i < tamanho; i += 9) {
-    const bloco = blocos.slice(i, i + 9).join("");
-    codigosFormatados.push(bloco.trim());
+  // Itera sobre os números
+  for (let i = 0; i < numeros.length; i++) {
+    // Verifica se o número tem 9 dígitos
+    if (numeros[i].length === 9) {
+      // Adiciona o número ao array de resultados
+      resultados.push(numeros[i]);
+    } else {
+      // Adiciona o número ao array de resultados, preenchendo com zeros
+      resultados.push(numeros[i].padStart(9, "0"));
+    }
   }
 
-  // Adicionar o texto formatado ao resultado
-  resultado.textContent = codigosFormatados.join(" | ");
-};
+  // Obtém o textarea de saída
+  const textoSaida = document.getElementById("textoSaida");
 
-enviar();
+  // Limpa o textarea de saída
+  textoSaida.value = "";
+
+  // Itera sobre os resultados
+  for (let i = 0; i < resultados.length; i++) {
+    // Adiciona o número a textarea de saída
+    textoSaida.value +=   resultados[i].replaceAll("\n", "/");
+  }
+// Adiciona uma opção de copiar o texto const
+  const botaoCopiar = document.createElement("button");
+  botaoCopiar.textContent = "Copiar";
+  botaoCopiar.addEventListener("click", function () {
+    // Copia o texto do textarea de saída
+    document.execCommand("copy");
+  });
+  textoSaida.appendChild(botaoCopiar);
+}
+
+// Adiciona um evento de clique ao botão enviar
+document.getElementById("botaoEnviar").addEventListener("click", separaNumeros);
